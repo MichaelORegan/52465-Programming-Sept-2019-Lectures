034139091af9883be04a20527e81076ed887e2e4
Michael O'Regan <G00364738@gmit.ie>
2018-10-07 20:54:31 +0100

    Adding Legend and Labels for x and y plots for legend

--- Matplotlib-Examples/2dplot2axes.py
+++ Matplotlib-Examples/2dplot2axes.py
@@ -5,11 +5,12 @@ x = np.arange(0.0, 10.0, 0.01)
 y = 3.0 * x + 1.0
 noise = np.random.normal(0.0, 1.0, len(x))
 
-plt.plot(x, y + noise, 'r.')
-plt.plot(x, y, 'b-')
+plt.plot(x, y + noise, 'r.', label='Actual') #label for legend
+plt.plot(x, y, 'b-', label='Model') #label for legend
 
 plt.title("Simple Plot")
 plt.xlabel("Weight")
 plt.ylabel("Mass")
+plt.legend() #link to labels above
 
 plt.show()
