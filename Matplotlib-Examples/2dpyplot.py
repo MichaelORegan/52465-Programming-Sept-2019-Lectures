0f33e256fb1d32ee31be903c4b8a66f07197d2c1
Michael O'Regan <G00364738@gmit.ie>
2018-10-07 13:19:54 +0100

    defaults straight line, plot 2 is squares on y, plot 3 shows points

--- Matplotlib-Examples/2dpyplot.py
+++ Matplotlib-Examples/2dpyplot.py
@@ -3,3 +3,11 @@ import matplotlib.pyplot as plt
 plt.plot([1, 2, 3, 4])
 plt.ylabel("Some Numbers")
 plt.show()
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+plt.ylabel("Some Numbers")
+plt.show()
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'b.')
+plt.ylabel("Some Numbers")
+plt.show()
