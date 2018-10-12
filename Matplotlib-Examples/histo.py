7e609589351b2659415f4abce1d91f46632f072f
Michael O'Regan <G00364738@gmit.ie>
2018-10-12 12:35:21 +0100

    Subplot top and bottom

--- Matplotlib-Examples/histo.py
+++ Matplotlib-Examples/histo.py
@@ -10,3 +10,13 @@ x = np.random.uniform(-3.0, 3.0, 10000)
 plt.hist(x)
 
 plt.show()
+
+plt.subplot(2, 1, 1)
+x = np.random.normal(0.0, 1.0, 10000)
+plt.hist(x)
+
+plt.subplot(2, 1, 2)
+x = np.random.uniform(-3.0, 3.0, 10000)
+plt.hist(x)
+
+plt.show()
